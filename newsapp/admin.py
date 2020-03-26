bed4717ba4f16facf56a855d7322bdb6b018b2d7
Gambhir jung karki <jungkarki23@gmail.com>
2020-03-26 14:54:35 +0545

    initial commit

---
+++ newsapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from newsapp.models import CategoryModel, PostModel, CommentModel
+
+admin.site.register(CategoryModel)
+admin.site.register(PostModel)
+admin.site.register(CommentModel)
+# Register your models here.
